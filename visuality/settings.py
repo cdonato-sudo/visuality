7713f8784282aee17a76d982818d98f00a8ceac7
cdonato-sudo <cdonato@pioix.edu.ar>
2025-12-11 18:39:51 -0300

    Update settings.py

--- visuality/settings.py
+++ visuality/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2-znd9$fzuznu^6r-eou*2f9bkx)k^noidts&zaui$6oz5!gm_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [visuality.onrender.com", "localhost", "127.0.0.1]
+ALLOWED_HOSTS = ["visuality.onrender.com", "localhost", "127.0.0.1]
 
 
 # Application definition
