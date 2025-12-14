4e12f46761a230946c7c8776f9ebd9f6ad5cd3fd
cdonato-sudo <cdonato@pioix.edu.ar>
2025-12-14 12:11:16 -0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,17 +1,10 @@
 from django.contrib import admin
-
 from .models import Video
 
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
-    """
-    Admin configuration for the ``Video`` model.  Lists a few useful
-    fields and allows searching by title and filename.  Ordering is
-    descending by the upload timestamp to ensure that the most recently
-    uploaded videos appear first in the list.
-    """
-
-    list_display = ("id", "titulo", "archivo", "fecha_subida", "usuario")
-    search_fields = ("titulo", "archivo")
-    ordering = ("-fecha_subida",)
+    list_display = ("id", "titulo", "usuario", "reproducido", "fecha_subida")
+    list_filter = ("reproducido", "fecha_subida")
+    search_fields = ("titulo", "usuario__username")
+    ordering = ("-id",)
