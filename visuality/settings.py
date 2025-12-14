57bae86b7bad36f9ff0ddeebb6eeba0bcbdd7600
cdonato-sudo <cdonato@pioix.edu.ar>
2025-12-14 11:55:03 -0300

    Update settings.py

--- visuality/settings.py
+++ visuality/settings.py
@@ -136,9 +136,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
-    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME", ""),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY", ""),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET", ""),
+    "RESOURCE_TYPE": "video",
 }
 
 STORAGES = {
