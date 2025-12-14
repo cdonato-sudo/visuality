cef5ea9feae97860adcc415135fdcb38365a55b8
cdonato-sudo <cdonato@pioix.edu.ar>
2025-12-14 13:00:15 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class Video(models.Model):
     titulo = models.CharField(max_length=255, blank=True)
-    archivo = models.FileField(upload_to='videos/')
+
+    # 👇 ESTE ES EL CAMBIO CLAVE
+    archivo = CloudinaryField(resource_type="video")
+
     fecha_subida = models.DateTimeField(auto_now_add=True)
-
-    # puede ser null (admin puede cargar videos)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-
-    # NUEVO: marca si ya se reprodujo (para volver al video base)
     reproducido = models.BooleanField(default=False)
 
-    def __str__(self) -> str:
-        return self.titulo or (self.archivo.name if self.archivo else "(sin título)")
+    def __str__(self):
+        return self.titulo or "(sin título)"
